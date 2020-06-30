7c1e08cbaf33ce1211f9ed228d0a85802ed705bb
Jonathan <nathanwrod@gmail.com>
2020-06-30 17:51:35 -0600

    Ultimas correcciones realizadas a la interfaz, depuracion de archivos

--- js/extras.js
+++ js/extras.js
@@ -27,3 +27,15 @@ $('[data-toggle="popover-click"]').popover({
   placement: 'bottom',
   content: function () { return $('#popover_content_list').html(); }
 });
+
+//prueba para botones
+var inicio = 1; //se inicializa una variable en 1
+
+function aumentar(){ 
+var cantidad = document.getElementById('cantidad').value = ++inicio; 
+}
+
+function disminuir(){ 
+
+var cantidad = document.getElementById('cantidad').value = --inicio; 
+}
